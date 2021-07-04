45cde86f3c3cabebea29e1fd83b5afc24baa9aef
Anirudh Ghayal <aghayal@codeaurora.org>
2021-07-04 21:16:21 +0800

    power: qpnp-smb5: Report TIME_TO_FULL_NOW
    Report TIME_TO_FULL_NOW from
    battery power-supply class property required by userspace.
    
    Change-Id: I97d37e8feb50e16d02a122211e90bdcede2b7f13
    Signed-off-by: Anirudh Ghayal <aghayal@codeaurora.org>
    Signed-off-by: HafizZiq <hafizuddinismail552@gmail.com>

--- drivers/power/supply/qcom/qpnp-smb5.c
+++ drivers/power/supply/qcom/qpnp-smb5.c
@@ -1617,6 +1617,7 @@ static enum power_supply_property smb5_batt_props[] = {
 	POWER_SUPPLY_PROP_RECHARGE_SOC,
 	POWER_SUPPLY_PROP_CHARGE_FULL,
 	POWER_SUPPLY_PROP_FORCE_RECHARGE,
+	POWER_SUPPLY_PROP_TIME_TO_FULL_NOW,
 	POWER_SUPPLY_PROP_FCC_STEPPER_ENABLE,
 	POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN,
 	POWER_SUPPLY_PROP_CHARGING_ENABLED,
@@ -1754,6 +1755,10 @@ static int smb5_batt_get_prop(struct power_supply *psy,
 		break;
 	case POWER_SUPPLY_PROP_FORCE_RECHARGE:
 		val->intval = 0;
+		break;
+	case POWER_SUPPLY_PROP_TIME_TO_FULL_NOW:
+		rc = smblib_get_prop_from_bms(chg,
+				POWER_SUPPLY_PROP_TIME_TO_FULL_NOW, val);
 		break;
 	case POWER_SUPPLY_PROP_FCC_STEPPER_ENABLE:
 		val->intval = chg->fcc_stepper_enable;
