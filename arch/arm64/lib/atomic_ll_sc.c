0939f56599fac5a36141581a754aca28d3f5a9c5
HafizZiq <hafizuddinismail552@gmail.com>
2021-07-13 05:30:40 +0800

    Revert "arm64: Move LSE atomics out from the alternatives framework"
    This reverts commit 53ad5b4171bf53aba6be3d3835bd948337dae58b.
    
    Signed-off-by: HafizZiq <hafizuddinismail552@gmail.com>

--- arch/arm64/lib/atomic_ll_sc.c
+++ arch/arm64/lib/atomic_ll_sc.c
@@ -1,3 +1,6 @@
+#include <asm/atomic.h>
+#define __ARM64_IN_ATOMIC_IMPL
+
 /*
  * Disarm the refcount checks in the out-of-line LL/SC routines. These are
  * redundant, given that the LSE callers already perform the same checks.
@@ -8,3 +11,5 @@
 #undef REFCOUNT_POST_CHECK_NEG_OR_ZERO
 #define REFCOUNT_POST_CHECK_NEG
 #define REFCOUNT_POST_CHECK_NEG_OR_ZERO   "csel %w[val], wzr, %w[val], eq\n"
+
+#include <asm/atomic_ll_sc.h>
