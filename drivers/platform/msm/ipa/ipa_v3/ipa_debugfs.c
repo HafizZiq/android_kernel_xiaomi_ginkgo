1c1cfaa5018a41d56c66541e095ba411e4cbbbcd
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-08-07 04:21:38 +0800

    Revert "msm: fix improper size checks"
    This reverts commit f23503ee08f064abbb0d1ea55d81392ed78e6f6d.
    
    Signed-off-by: HafizZiq <hafizuddinismail552@gmail.com>

--- drivers/platform/msm/ipa/ipa_v3/ipa_debugfs.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa_debugfs.c
@@ -158,7 +158,7 @@ static ssize_t ipa3_write_ep_holb(struct file *file,
 	unsigned long missing;
 	char *sptr, *token;
 
-	if (sizeof(dbg_buff) < count)
+	if (sizeof(dbg_buff) < count + 1)
 		return -EFAULT;
 
 	missing = copy_from_user(dbg_buff, buf, min(sizeof(dbg_buff), count));
@@ -201,7 +201,7 @@ static ssize_t ipa3_write_ep_reg(struct file *file, const char __user *buf,
 	unsigned long missing;
 	s8 option = 0;
 
-	if (sizeof(dbg_buff) < count)
+	if (sizeof(dbg_buff) < count + 1)
 		return -EFAULT;
 
 	missing = copy_from_user(dbg_buff, buf, min(sizeof(dbg_buff), count));
@@ -340,7 +340,7 @@ static ssize_t ipa3_write_keep_awake(struct file *file, const char __user *buf,
 	s8 option = 0;
 	uint32_t bw_mbps = 0;
 
-	if (sizeof(dbg_buff) < count)
+	if (sizeof(dbg_buff) < count + 1)
 		return -EFAULT;
 
 	missing = copy_from_user(dbg_buff, buf, min(sizeof(dbg_buff), count));
@@ -1583,7 +1583,7 @@ static ssize_t ipa3_write_dbg_cnt(struct file *file, const char __user *buf,
 		return -EPERM;
 	}
 
-	if (sizeof(dbg_buff) < count)
+	if (sizeof(dbg_buff) < count + 1)
 		return -EFAULT;
 
 	missing = copy_from_user(dbg_buff, buf, min(sizeof(dbg_buff), count));
@@ -2607,7 +2607,7 @@ static ssize_t ipa3_clear_active_clients_log(struct file *file,
 	unsigned long missing;
 		s8 option = 0;
 
-	if (sizeof(dbg_buff) < count)
+	if (sizeof(dbg_buff) < count + 1)
 		return -EFAULT;
 
 	missing = copy_from_user(dbg_buff, ubuf, min(sizeof(dbg_buff), count));
@@ -2629,7 +2629,7 @@ static ssize_t ipa3_enable_ipc_low(struct file *file,
 	unsigned long missing;
 	s8 option = 0;
 
-	if (sizeof(dbg_buff) < count)
+	if (sizeof(dbg_buff) < count + 1)
 		return -EFAULT;
 
 	missing = copy_from_user(dbg_buff, ubuf, min(sizeof(dbg_buff), count));
